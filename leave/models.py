6a46568d5487b94150d532036b8c0591e78e5246
Peng Hailin <unisko@gmail.com>
2021-11-12 09:25:10 +0000

    updated.

--- leave/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-from viewflow.models import Process
-
-
-class LeaveProcess(Process):
-    reason = models.CharField(max_length=150)
-    approved = models.BooleanField(default=False)
